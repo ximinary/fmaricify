57bb9d89fd264b836351d3fc0539145310f3a368
ximinary <serjik2706@gmail.com>
2026-03-11 13:51:46 +0100

    js try 6

--- script.js
+++ script.js
@@ -2,69 +2,28 @@ let data;
 let schedules = [];
 
 const days = {
-    "pon":0,
-    "uto":1,
-    "sre":2,
-    "cet":3,
-    "pet":4
+  "pon":0,
+  "uto":1,
+  "sre":2,
+  "cet":3,
+  "pet":4
 };
 
-fetch("timetable.json")
+fetch("out.json")
 .then(r => r.json())
-.then(j => {
-    data = j.predmeti;
-});
+.then(j => data = j.predmeti);
 
-function slot(vreme)
-{
-    let d = vreme.substring(0,3);
-    let h = parseInt(vreme.substring(3));
+function getSelections() {
 
-    return days[d]*100 + h;
-}
-
-function conflict(schedule, termin)
-{
-    for (let t of termin.termini)
-    {
-        let s = slot(t.vreme);
-
-        if (schedule.has(s))
-            return true;
-    }
-
-    return false;
-}
-
-function addTermin(schedule, termin, predmet, tip)
-{
-    let ns = new Map(schedule);
-
-    for (let t of termin.termini)
-    {
-        let s = slot(t.vreme);
-
-        ns.set(s,{
-            predmet: predmet,
-            tip: tip,
-            soba: t.soba
-        });
-    }
-
-    return ns;
-}
-
-function getSelections()
-{
     let result = [];
 
     data.forEach(predmet => {
 
-        let c = document.querySelector(
+        let checkbox = document.querySelector(
             "input[name='" + predmet.naziv + "_c']"
         );
 
-        if (!c || !c.checked)
+        if (!checkbox || !checkbox.checked)
             return;
 
         let p = document.querySelector(
@@ -91,16 +50,63 @@ function getSelections()
     return result;
 }
 
-function subjectOptions(sel)
-{
-    let options = [[]];
+function slot(vreme) {
 
-    function add(type, teacherChoice, list)
-    {
+    let d = vreme.substring(0,3);
+    let h = parseInt(vreme.substring(3));
+
+    return days[d]*100 + h;
+}
+
+function conflict(schedule, termin) {
+
+    for (let t of termin.termini) {
+
+        let s = slot(t.vreme);
+
+        if (schedule.has(s))
+            return true;
+    }
+
+    return false;
+}
+
+function addTermin(schedule, termin, predmet, tip) {
+
+    let newSched = new Map(schedule);
+
+    for (let t of termin.termini) {
+
+        let s = slot(t.vreme);
+
+        newSched.set(s,{
+            predmet: predmet,
+            tip: tip,
+            soba: t.soba
+        });
+    }
+
+    return newSched;
+}
+
+function build(i, selections, schedule) {
+
+    if (i === selections.length) {
+
+        schedules.push(schedule);
+        return;
+    }
+
+    let sel = selections[i];
+
+    tryTip("p", sel.p, sel.predmet.p);
+    tryTip("v", sel.v, sel.predmet.v);
+    tryTip("k", sel.k, sel.predmet.k);
+
+    function tryTip(tip, teacherChoice, list) {
+
         if (!list || teacherChoice === "-")
             return;
-
-        let newOptions = [];
 
         list.forEach(t => {
 
@@ -108,65 +114,25 @@ function subjectOptions(sel)
                 teacherChoice !== t.nastavnik)
                 return;
 
-            options.forEach(o => {
-                newOptions.push([...o,{tip:type,termin:t}]);
-            });
+            if (conflict(schedule, t))
+                return;
 
+            let ns = addTermin(
+                schedule,
+                t,
+                sel.predmet.naziv,
+                tip
+            );
+
+            build(i+1, selections, ns);
         });
 
-        options = newOptions;
     }
 
-    add("p", sel.p, sel.predmet.p);
-    add("v", sel.v, sel.predmet.v);
-    add("k", sel.k, sel.predmet.k);
-
-    if (options.length === 0)
-        options = [[]];
-
-    return options;
 }
 
+function generate() {
 
-
-function build(i, selections, schedule)
-{
-    if (i === selections.length)
-    {
-        schedules.push(schedule);
-        return;
-    }
-
-    let sel = selections[i];
-    let options = subjectOptions(sel);
-
-    options.forEach(opt => {
-
-        let sched = new Map(schedule);
-        let bad = false;
-
-        opt.forEach(item => {
-
-            if (conflict(sched, item.termin))
-                bad = true;
-            else
-                sched = addTermin(
-                    sched,
-                    item.termin,
-                    sel.predmet.naziv,
-                    item.tip
-                );
-
-        });
-
-        if (!bad)
-            build(i+1, selections, sched);
-
-    });
-}
-
-function generate()
-{
     schedules = [];
 
     let selections = getSelections();
@@ -176,76 +142,33 @@ function generate()
     showSchedules();
 }
 
-function showSchedules()
-{
+function showSchedules() {
+
     let out = document.getElementById("results");
 
     out.innerHTML = "";
 
-    if (schedules.length === 0)
-    {
-        out.innerHTML = "No valid schedules.";
-        return;
-    }
-
-    schedules.forEach((s,i) => {
+    schedules.forEach((s, i) => {
 
         let div = document.createElement("div");
 
-        let title = document.createElement("h3");
-        title.innerText = "Schedule " + (i+1);
+        div.innerHTML = "<h3>Schedule "+(i+1)+"</h3>";
 
-        div.appendChild(title);
+        s.forEach((v,k) => {
 
-        div.appendChild(drawTable(s));
+            let d = Math.floor(k/100);
+            let h = k%100;
+
+            div.innerHTML +=
+                ["pon","uto","sre","cet","pet"][d] +
+                " " + h +
+                " - " + v.predmet +
+                " ("+v.tip+") " +
+                v.soba +
+                "<br>";
+        });
 
         out.appendChild(div);
-
     });
-}
 
-function drawTable(schedule)
-{
-    let table = document.createElement("table");
-    table.border = "1";
-
-    let head = document.createElement("tr");
-
-    head.innerHTML =
-        "<th></th><th>Pon</th><th>Uto</th><th>Sre</th><th>Cet</th><th>Pet</th>";
-
-    table.appendChild(head);
-
-    for (let h=8; h<=20; h++)
-    {
-        let tr = document.createElement("tr");
-
-        let th = document.createElement("th");
-        th.innerText = h;
-
-        tr.appendChild(th);
-
-        for (let d=0; d<5; d++)
-        {
-            let td = document.createElement("td");
-
-            let key = d*100 + h;
-
-            if (schedule.has(key))
-            {
-                let v = schedule.get(key);
-
-                td.innerHTML =
-                    v.predmet +
-                    "<br>(" + v.tip + ")" +
-                    "<br>" + v.soba;
-            }
-
-            tr.appendChild(td);
-        }
-
-        table.appendChild(tr);
-    }
-
-    return table;
 }
